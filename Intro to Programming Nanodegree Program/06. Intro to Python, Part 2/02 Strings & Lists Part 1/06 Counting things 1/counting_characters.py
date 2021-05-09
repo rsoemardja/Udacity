a21c3c200b00b4fa7dbf72d14cca55d42f64647f
Rama Komarudin Soemardja <rsoemardja@gmail.com>
2021-05-09 23:20:42 +1200

    sunday

--- Intro to Programming Nanodegree Program/06. Intro to Python, Part 2/02 Strings & Lists Part 1/06 Counting things 1/counting_characters.py
+++ Intro to Programming Nanodegree Program/06. Intro to Python, Part 2/02 Strings & Lists Part 1/06 Counting things 1/counting_characters.py
@@ -1,2 +1,10 @@
+#for ch in "bonobos":
+#    if ch == "o":
+
+count = 0
+
 for ch in "bonobos":
     if ch == "o":
+        count = count + 1
+
+print(count)
---
+++ Intro to Programming Nanodegree Program/08. Intro to JavaScript/02 Data Types & Variables/Yosa Buson.js
@@ -0,0 +1,13 @@
+/*
+ * Programming Quiz: Yosa Buson (2-6)
+ */
+
+/*
+ * QUIZ REQUIREMENTS
+ * 1. Your code should have a variable `haiku`
+ * 2. Your code should use string concatenation
+ * 3. Your poem should match the famous haiku poem
+ */
+
+var haiku = "Blowing from the west" + "\nFallen leaves gather" + "\nIn the east."/* concatenate the strings here */
+    console.log(haiku);
